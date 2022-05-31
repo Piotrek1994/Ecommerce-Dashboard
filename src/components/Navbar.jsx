60047c3ab2c7bb7eb6edc36aefa0149b26c15f1e
Piorek1994 <pprus94@gmail.com>
2022-05-31 18:38:31 +0200

    Fixing problem with buttons

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -63,7 +63,7 @@ useEffect(() => {
 
           <p>
             <span className='text-gray-400 text-14'>Hi, </span> {' '}
-            <span className='text-gray-400 font-bold ml-1 text-14'>Michael</span>
+            <span className='text-gray-400 font-bold ml-1 text-14'>Piotr</span>
           </p>
           <MdKeyboardArrowDown className="text-gray-400 text-14" />
 
